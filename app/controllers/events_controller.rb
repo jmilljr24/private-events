ec9a50d41410f252662d2ad7c4c1c66673fb148f
jmilljr24 <jmilljr24@gmail.com>
2023-08-21 10:35:28 -0400

    Add event when user logged in. Add user_id to database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,8 +20,8 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
-
+    # @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
     respond_to do |format|
       if @event.save
         format.html { redirect_to event_url(@event), notice: 'Event was successfully created.' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_133431) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_143403) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
