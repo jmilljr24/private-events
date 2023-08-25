0dbf535ddbfb51508df2c83f26162283d6c548d1
jmilljr24 <jmilljr24@gmail.com>
2023-08-25 10:42:25 -0400

    Add enrollments db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_143403) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_143132) do
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_enrollments_on_event_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
@@ -32,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_143403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "enrollments", "events"
+  add_foreign_key "enrollments", "users"
 end
