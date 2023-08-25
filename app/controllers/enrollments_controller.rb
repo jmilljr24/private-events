37e1ca0d64a5921081757da6c7fd285b31ff95f6
jmilljr24 <jmilljr24@gmail.com>
2023-08-25 15:11:18 -0400

    Add attend and unattend option for current user

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -12,4 +12,12 @@ class EnrollmentsController < ApplicationController
       redirect_to @event, alert: 'You are already attending this event.'
     end
   end
+
+  def destroy
+    @event = Event.find(params[:id])
+
+    attendee = Enrollment.find_by(user_id: current_user.id, event_id: @event.id)
+    attendee.destroy
+    redirect_to @event, notice: 'You are no longer attending this event!'
+  end
 end
